1abfb0e42bc10e8b5de44b841398c96e1e7e18bd
Filip Krzemień <filipos2003@gmail.com>
2023-06-14 21:23:14 +0200

    Basic header styling and added some scss variables

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,12 +1,17 @@
 import Link from 'next/link';
+import { Kanit } from 'next/font/google';
 import classes from './Header.module.scss';
 
+const kanit = Kanit({
+    subsets:['latin'] ,
+    weight:['700'] , 
+})
 
 const Header = () => {
 
     return(
         <header className={classes.header}>
-            <h3>Studier</h3>
+            <h3 className={kanit.className}>Studier</h3>
             <nav className={classes['login-options']}>
             <Link href='/login'>Login</Link> 
             <Link href='/register'>Sign Up</Link> 
