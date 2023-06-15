285665fcfce98b61b879c3fa73ee3b0ca6ff8c56
Filip Krzemień <filipos2003@gmail.com>
2023-06-15 17:08:31 +0200

    Added theme changer

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,6 +1,11 @@
+"use client";
 import Link from "next/link";
 import { Kanit } from "next/font/google";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
+import Brightness3Icon from "@mui/icons-material/Brightness3";
 import classes from "./Header.module.scss";
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
 
 const kanit = Kanit({
   subsets: ["latin"],
@@ -8,9 +13,42 @@ const kanit = Kanit({
 });
 
 const Header = () => {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
   return (
     <header className={classes.header}>
-      <h3 className={kanit.className}>Studier</h3>
+      <div className={classes.logo}>
+        <h3 className={kanit.className}>Studier</h3>
+        {theme === "light" && (
+          <WbSunnyIcon
+            fontSize="large"
+            style={{ color: "orange" }}
+            onClick={() => {
+              setTheme("dark");
+            }}
+            className={classes.icon}
+          />
+        )}
+        {theme === "dark" && (
+          <Brightness3Icon
+            fontSize="large"
+            style={{ color: "white" }}
+            onClick={() => {
+              setTheme("light");
+            }}
+            className={classes.icon}
+          />
+        )}
+      </div>
       <nav className={classes["login-options"]}>
         <Link href="/login">Login</Link>
         <Link href="/register">Sign Up</Link>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
+import Providers from "@/context/ThemeProvider";
 import Header from "./components/Header/Header";
 import { Inter } from "next/font/google";
 import "./globals.scss";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,12 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1 ,  minimum-scale=1 " />
-      </Head>
       <body className={inter.className}>
+        <Providers>
         <Header />
         {children}
+        </Providers>
       </body>
     </html>
   );
