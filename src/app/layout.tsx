d735173cb59086c0ee344ca3d49429ed5ef99112
Filip Krzemień <filipos2003@gmail.com>
2023-06-15 14:32:47 +0200

    Responsive design improvment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header";
 import { Inter } from "next/font/google";
 import "./globals.scss";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1 ,  minimum-scale=1 " />
+      </Head>
       <body className={inter.className}>
         <Header />
         {children}
