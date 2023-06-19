4b2089308a99ec581c5f365cc7701c34f434a8a9
Filip Krzemień <filipos2003@gmail.com>
2023-06-19 14:07:00 +0200

    Mobile nav imrovments

--- src/app/components/Header/MobileNav.tsx
+++ src/app/components/Header/MobileNav.tsx
@@ -1,11 +1,20 @@
+"use client";
 import Link from "next/link";
+import LoginIcon from '@mui/icons-material/Login';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import classes from "./MobileNav.module.scss";
 
 const MobileNav = () => {
   return (
     <nav className={classes.nav}>
-      <Link href="/login">Login</Link>
-      <Link href="/register">Sign Up</Link>
+        <div className={classes.item}>
+            <LoginIcon />
+        <Link href="/login">Login</Link>
+        </div>
+        <div className={classes.item}>
+            <PersonAddIcon />
+        <Link href="/register">Sign Up</Link>
+        </div>
     </nav>
   );
 };
